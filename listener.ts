a5b01b0923b189a0e50653516918602fa4e471df
toddinpal <toddjlittle@gmail.com>
2021-09-14 17:24:42 -0600

    Updated comments and added additional console log

--- listener.ts
+++ listener.ts
@@ -7,12 +7,19 @@ const serverPort = process.env.serverPort;
 const APIKey = process.env.APIKey;
 if (!serverPort || !APIKey) {
     console.log(`Missing required environment variables: serverPort="${serverPort}" APIKey="${APIKey}"`);
+    console.log('Please see https://github.com/toddinpal/syslog-interceptor for more details on how to use.');
     process.exit(1);
 }
 const nextServerAddr = process.env.nextServerAddr;
 const nextServerPort = process.env.nextServerPort;
-if (nextServerAddr == undefined || nextServerPort == undefined) console.log('Not forwarding messages');
+if (nextServerAddr == undefined || nextServerPort == undefined) {
+    console.log("Not forwarding messages because either nextServerAddr and/or nextServerPort isn't set.");
+    console.log('Please see https://github.com/toddinpal/syslog-interceptor for more details on how to use.');
+} else {
+    console.log('Passing syslog messages to: "$(nextServerAddress)":"$(nextServerPort)"')
+}
 
+// The structure of a syslog message
 interface SysLogMessage<T> {
     originalMessage: string
     pri: string
@@ -34,6 +41,7 @@ interface SysLogMessage<T> {
     structuredData: T[]
 }
 
+// Synology structured data in a syslog message.  Gleaned by examination.  Wish Synology would document these.
 interface SynologyStructuredData {
     '$id': string
     event_id: string
@@ -48,11 +56,11 @@ interface SynologyStructuredData {
     sequenceId: string
 }
 
-
+// Create UDP sockets for this server and for the server to relay the messages to
 const server = dgram.createSocket('udp4');
 const client = dgram.createSocket('udp4');
 
-// Keep a list of the reported IPs for the day
+// Keep a list of the reported IPs for the day as we only want to report an IP address once a day per AbuseIPDB
 const reportedSet = new Set<string>();
 
 // Create a job to clear the list of reported IPs every midnight
@@ -64,19 +72,25 @@ server.on('error', (err) => {
     server.close();
 });
 
+//  For each syslog message we receive:
 server.on('message', (msg, rinfo) => {
     console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 
-    // Forward the message to the intended syslog server
+    // Forward the message to the intended syslog server, should probably check return status
     if (nextServerAddr != undefined && nextServerPort != undefined) client.send(msg, +nextServerPort, nextServerAddr);
 
+    // Parse the syslog message
     const parsedMessage = <SysLogMessage<SynologyStructuredData>>parser(msg + "");
 
+    // Currently this is set up to make sure the syslog message contains the strings "failed to log in via" and "due to authorization failure." to
+    // determine if the syslog message is a login failure that should be reported.  This could easily be expanded to include other messages, although
+    // the parsing logic below to generate the AbuseIPDB report might need to be updated as well.
+    //
     // Check to verify it's a login failure
     if (!parsedMessage.message.includes('failed to log in via')) return;
     if (!parsedMessage.message.includes('due to authorization failure.')) return;
 
-    // Temporary fix for me
+    // Temporary fix for me as one of my DSMs is reporting the other one for login failures
     if (parsedMessage.structuredData[0].arg_1 == 'little') return;
 
     // This assumes that there is only a single structured data entry in the message
@@ -89,6 +103,7 @@ server.on('message', (msg, rinfo) => {
     }
     reportedSet.add(reportedIP);
 
+    // Report the abuse.  Currently just reports all abuse as category 18 - Brute-Force
     reportAbuse(reportedIP, '18', parsedMessage.message, APIKey);
 
 });
--- report.ts
+++ report.ts
@@ -1,3 +1,7 @@
+//
+// This routine is used to send an abuse report to AbuseIPDB based upon their REST API.  You need to have an APIKey
+// from AbuseIPDB to use this function.
+//
 import fetch from "node-fetch"
 interface ReportResponse {
     data: ReportResponseData
@@ -6,10 +10,6 @@ interface ReportResponseData {
     ipAddress: string,
     abuseConfidenceScore: number
 }
-// url.searchParams.set("ip", "118.212.69.142");
-// url.searchParams.set("categories", "18");
-// url.searchParams.set("comment", "Test report to verify API usage");
-
 
 export async function reportAbuse(ip: string, categories: string, comment: string, APIkey: string) {
     const url = new URL("https://api.abuseipdb.com/api/v2/report");
